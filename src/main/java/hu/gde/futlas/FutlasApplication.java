5dc0a51e66eb2e86b6aa664e74fb30c8051f74d0
uzsito <mark.molnar.hu@gmail.com>
2025-12-09 15:20:19 +0100

    Initial commit

---
+++ src/main/java/hu/gde/futlas/FutlasApplication.java
@@ -0,0 +1,13 @@
+package hu.gde.futlas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutlasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FutlasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hu/gde/futlas/FutlasApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.gde.futlas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutlasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
